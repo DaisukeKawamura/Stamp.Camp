cddfb48b6f1484b675ef422f5664d128a02590e0
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2018-06-02 21:01:06 +0900

    スタンプアプリ

---
+++ app/src/main/java/android/lifeistech/com/decopiccamp/ActionBarActivity.java
@@ -0,0 +1,11 @@
+package android.lifeistech.com.decopiccamp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kawamuradaisuke on 2018/06/02.
+ */
+
+@Deprecated
+public class ActionBarActivity extends AppCompatActivity {
+}
